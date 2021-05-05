caada6053812a9d9710606291d909518001648b1
Marissa E. E <>
2021-05-05 21:12:43 +0000

    Add button for javascript

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function popup() {
 function onsite(){
   document.getElementById("text").innerHTML="something appears";
 }
+function signUp(){
+  var txt; 
+  var person = prompt ("Please enter your name: ", "Adriana Guerrero"); //don't need anything after comma
+  if(person == null || person == ""){
+    txt = "You are not signed up.";
+  }
+  else{
+    txt = "Thank you " + person + "! You have successfully signed up!";
+  }
+  document.getElementById("message").innerHTML = txt;
+}
