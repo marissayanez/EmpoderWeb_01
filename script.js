e51847b55d82c383965f9b15e9b640566cb147ff
Marissa E. E <>
2021-05-06 00:36:51 +0000

    animation fox

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function popup() {
 function onsite(){
   document.getElementById("text").innerHTML="something appears";
 }
+
 function signUp(){
   var txt; 
   var person = prompt ("Please enter your name: ", "Adriana Guerrero"); //don't need anything after comma
@@ -15,3 +16,26 @@ function signUp(){
   }
   document.getElementById("message").innerHTML = txt;
 }
+
+function init() 
+{
+  image = document.getElementById("fox");
+  image.style.position = "relative"
+}
+var id = null;
+function myMove() {
+  var elem = document.getElementById("fox");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      // elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+    }
+  }
+}
+window.onload = init;
